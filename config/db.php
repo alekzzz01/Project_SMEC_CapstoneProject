24af2640d890a926d847cb95547a527705c78577
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2024-11-24 14:02:16 +0800

    PRG pattern for resubmission

--- config/db.php
+++ config/db.php
@@ -8,8 +8,8 @@ $connection = new mysqli($hostName, $dbAdmin, $dbPassword, $dbName);
 
 if ($connection->connect_error) {
     die("Something went wrong; " . $connection->connect_error);
-} else {
-    echo "Connected to database successfully.";
+// } else {
+//     echo "Connected to database successfully.";
 }
 
 ?>
