68c515c15f25929f39ed3eaf5d4ff282a025fd2f
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2025-01-13 21:18:15 +0800

    adjustments for admission approvaltable

--- dist/admin/tables/admission_approvalTable.php
+++ dist/admin/tables/admission_approvalTable.php
@@ -149,6 +149,8 @@ ob_end_flush(); // End output buffering
                     <th>Submission Date</th>
                     <th>Status</th>
                     <th>Action</th>
+                    <th></th>
+                  
                 </tr>
             </thead>
             <tbody>
@@ -164,33 +166,53 @@ ob_end_flush(); // End output buffering
                         <td><?php echo $row['year_level']; ?></td>
                         <td><?php echo $row['created_at']; ?></td>
                         <td><?php echo $row['phone']; ?></td>
+
+                
+
                         <td>
-                        <form method="POST" >
+                        
+                            <form method="POST" >
                                 <input type="hidden" name="student_id" value="<?php echo $row['id']; ?>">
                              
                                 <div class="dropdown  dropdown-left dropdown-end">
-                                        <div tabindex="0" role="button" class="btn btn-circle btn-ghost btn-xs">
+                                    
+                                        <div tabindex="0" role="button" class="btn btn-circle btn-ghost btn-sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                         </svg>
 
                                         </div>
-
+                                    
                                     <div
                                             tabindex="1"
                                             class="menu dropdown-content bg-base-100 z-[1] rounded-md mt-4 w-52 p-2 shadow">
                                             <li>  <button type="submit" name="approve" class="text-sm font-semibold">Approve</button></li>
                                             <li>  <button type="submit" name="reject" class="text-sm font-semibold">Reject</button></li>
-                                            <li>  <button onclick="send_email.showModal()" class="text-sm font-semibold">Send Email </button></li>
+                                          
     
                                     </div>
                                 </div>
-              
+
+                               
             
                               
                             </form>
+
+                        
+                        
                         </td>
+                        <td>
+                            <div class="tooltip tooltip-left" data-tip="Send Email to Applicant">
+ 
+                            <button onclick="send_email.showModal()" class="btn btn-circle btn-ghost btn-sm"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                                </svg>
+                            </button>
+
+                        
+                            </div>
                         </td>
+                     
 
                     </tr>
                 <?php endwhile; ?>
@@ -214,50 +236,50 @@ ob_end_flush(); // End output buffering
 
 
         <dialog id="send_email" class="modal modal-bottom sm:modal-middle">
-        <div class="modal-box p-0">
+            <div class="modal-box p-0">
 
-            <h3 class="font-medium bg-amber-300 p-4">Email Applicant</h3>
+                <h3 class="font-medium bg-amber-300 p-4">Email Applicant</h3>
 
-            <form method="dialog">
-                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-            </form>
+                <form method="dialog">
+                    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
 
-            <form action="" class="p-4" method="POST">
+                <form action="" class="p-4" method="POST">
 
-                    <div class="space-y-5">
-                    
-                        <label  class="block font-regular text-gray-400">To: <span class=" text-black"> email of student </span></label>
-                        <label class="block font-regular text-gray-400">From: <span class=" text-black"> SMEC_2025@gmail.com </span></label>
-
-                        <div class="divider"></div>
-
-                        <label class="input input-bordered flex items-center gap-2">
-                      
-                        <input type="text" class="grow" placeholder="Subject" />
-                        </label>
-
-                        <textarea class="textarea textarea-bordered w-full" placeholder="Body"></textarea>
-
+                        <div class="space-y-5">
                         
+                            <label  class="block font-regular text-gray-400">To: <span class=" text-black"> email of student </span></label>
+                            <label class="block font-regular text-gray-400">From: <span class=" text-black"> SMEC_2025@gmail.com </span></label>
+
+                            <div class="divider"></div>
+
+                            <label class="input input-bordered flex items-center gap-2">
                         
-                    </div>
+                            <input type="text" class="grow" placeholder="Subject" />
+                            </label>
 
-                
-                
-                    <div class="modal-action col-span-2">
+                            <textarea class="textarea textarea-bordered w-full" placeholder="Body"></textarea>
+
+                            
+                            
+                        </div>
+
                     
-                    <button type="submit" name="createUser" class="py-1.5 px-3 rounded-md text-sm transition-colors bg-green-500 hover:bg-green-700 text-white border border-green-500 hover:border-green-700">Send Email</button>
-                    <button type="submit" name="createUser" class="py-1.5 px-3 rounded-md text-sm transition-colors bg-red-500 hover:bg-red-700 text-white border border-red-500 hover:border-red-700">Draft</button>
+                    
+                        <div class="modal-action col-span-2">
+                        
+                        <button type="submit" name="createUser" class="py-1.5 px-3 rounded-md text-sm transition-colors bg-green-500 hover:bg-green-700 text-white border border-green-500 hover:border-green-700">Send Email</button>
+                        <button type="submit" name="createUser" class="py-1.5 px-3 rounded-md text-sm transition-colors bg-red-500 hover:bg-red-700 text-white border border-red-500 hover:border-red-700">Draft</button>
 
-                    </div>
-     
+                        </div>
+        
 
-            </form>
+                </form>
 
-        </div>
+            </div>
            
           
-    </dialog>
+        </dialog>
 
 </body>
 </html>
