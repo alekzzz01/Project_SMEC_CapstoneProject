6654099089219b33eaca1701caf918c497a2e78c
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2025-01-29 19:07:51 +0800

    Add Term page added

---
+++ dist/admin/class_term.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Term</title>
+
+    <link rel="stylesheet" href="../../assets/css/styles.css">
+     
+     <script src="../../assets/js/script.js"></script>
+ 
+  
+     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  
+     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+  
+     <script src="https://cdn.tailwindcss.com"></script>
+ 
+     <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+     <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
+ 
+     <link href="https://cdn.jsdelivr.net/npm/heroicons@1.0.6/dist/heroicons.min.css" rel="stylesheet">
+ 
+  
+     <link href='https://unpkg.com/boxicons/css/boxicons.min.css' rel='stylesheet'>
+ 
+      
+     <html data-theme="light"></html>
+
+
+    <!-- DataTables CSS (Hover Styling) -->
+    <link href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.css" rel="stylesheet">
+  
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/2.2.1/js/dataTables.js"></script>
+
+
+</head>
+<body class="flex min-h-screen">
+
+    <?php include('./components/sidebar.php'); ?>
+
+
+    <div class="flex flex-col w-full">
+
+    <?php include('./components/navbar.php'); ?>
+
+        <div class="p-6 bg-[#f2f5f8] h-full">
+
+            <div>
+                    <h1 class="text-lg font-medium mb-1">Term</h1>
+            </div>
+
+            <div class="border border-gray-300 rounded bg-white mt-7">
+
+                <h1 class="font-semibold p-5 bg-blue-50 rounded-t text-blue-600">Add Term</h1>
+
+                <form class="p-5 space-y-6">
+
+                    <div class="grid grid-cols-1 lg:grid-cols-5 gap-6">
+                        <div>
+                            <label class="text-gray-800 text-sm font-medium mb-2 block">Input Year</label>
+                            <div class="relative flex items-center">
+                            <input name="" type="text" class="bg-gray-50 w-full text-gray-800 input input-bordered" placeholder="e.g. 2024-2025"/>
+                            </div>
+                                       
+                        </div>
+
+                    </div>
+                    
+                    <div class=" flex items-center justify-center">
+                        <button type="submit" name="submitForm" class=" py-3 px-16 text-sm rounded-md text-white font-medium tracking-wide bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 focus:ring-offset-blue-50 transition-colors group">Submit</button>
+                    </div>
+                        
+
+                        
+
+                </form>
+
+
+            </div>
+
+
+            <div class="border border-gray-300 rounded bg-white mt-3.5 p-6">
+
+                <table id="example" class="min-w-full divide-y divide-gray-200">
+                    <thead class="border border-gray-300 text-sm">
+                        <tr>
+                            <th class="py-3 px-4 text-left">School Year ID</th>
+                            <th class="py-3 px-4 text-left">School Year</th>
+                            <th class="py-3 px-4 text-left">Status</th>
+                            <th class="py-3 px-4 text-left">Action</th>
+                        </tr>
+
+                    </thead>
+
+                    <tbody class="divide-y divide-gray-200 border border-gray-300">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">1</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">2024-2025</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">Open</td>
+                 
+                            <td>
+                                    
+                                    <form method="POST" >
+                                        <input type="hidden" name="student_id" value="<?php echo $row['id']; ?>">
+                                    
+                                        <button type='submit' name='approve' class='text-green-600 text-sm hover:underline'>[Open]</button>
+                                        <button type='submit' name='reject' class='text-red-500 text-sm hover:underline'>[Close]</button>
+                                                         
+                                    </form>
+
+                                
+                                
+                            </td>
+                        </tr>
+
+                    </tbody>
+
+                </table>
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+    
+</body>
+</html>
+
+<script>
+    $(document).ready(function () {
+        var table = $('example').DataTable ({
+            searching: true, // Enables the search box
+            paging: true,    // Enables pagination
+            ordering: true,  // Enables column sorting
+            info: true,      // Displays table information
+        });
+
+    });
+</script>
