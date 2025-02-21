25de43d78aeab44db766b21b2a989a30690cc5ed
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2025-02-21 19:49:07 +0800

    added news and events, added view news

---
+++ newsAndevents.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News & Events</title>
+
+    <link rel="stylesheet" href="./assets/css/styles.css">
+
+    <script src="./assets/js/script.js"></script>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script src="https://unpkg.com/@heroicons/react@2.0.16/dist/outline/index.js" type="module"></script>
+
+    <link href='https://unpkg.com/boxicons/css/boxicons.min.css' rel='stylesheet'>
+
+    <html data-theme="light">
+
+    </html>
+
+</head>
+
+<body>
+
+
+    <?php include 'publicnavbar.php' ?>
+
+
+    <section class="py-36 px-4 lg:px-12 bg-blue-800 flex items-center justify-center">
+
+
+        <div>
+            <h1 class="text-4xl lg:text-6xl font-semibold text-white mb-4 shadow-sm">News & Events</h1>
+        </div>
+
+
+
+    </section>
+
+    <section class="py-12 px-4">
+        <div class="mx-auto max-w-7xl space-y-12">
+
+            <div class="p-4 lg:p-12 text-center bg-gray-50 rounded-2xl">
+                <p class="mb-4 font-medium tracking-[0.2rem] text-gray-600">WELCOME TO NEWS AND EVENTS</p>
+                <h1 class="text-lg lg:text-3xl font-semibold leading-10">Stay Updated ✏️ with the Latest <span class="text-blue-600">School News! 📢 </span>,
+                    <br>
+                    Discover Exciting <span class="text-blue-600">Events 📅</span> and <span class="text-blue-600">Announcements! 🎉</span>
+                </h1>
+
+
+            </div>
+
+
+            <div class="grid grid-cols-1 lg:grid-cols-3">
+                <img src="https://bloximages.chicago2.vip.townnews.com/thetimestribune.com/content/tncms/assets/v3/editorial/2/ed/2edbebde-ec11-11ea-9a29-17cf173ad57d/5f4dd76e4af8c.image.jpg?resize=400%2C266" alt="" class="rounded-md w-full h-80 object-cover">
+
+                <div class="space-y-4 py-4 lg:pl-12 lg:py-8 flex flex-col justify-center col-span-2">
+                    <p class=" text-gray-600">Department · <span>12 minutes ago</span></p>
+
+                    <h1 class="text-xl lg:text-4xl font-bold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                    <p class="text-gray-600 line-clamp-3">
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                    </p>
+
+                    <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                </div>
+            </div>
+
+
+            <div class="news">
+                <div class="flex items-center justify-between">
+                    <h1 class="text-3xl font-semibold">Recent News</h1>
+
+
+                    <a href="#" class="font-medium text-blue-700 transition-colors inline-flex items-center gap-2">
+                        <span>See all</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </a>
+
+
+                </div>
+
+                <div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+
+                    <a href="./news/viewNews.php" class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </a>
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+
+            </div>
+
+            <div class="events">
+                <div class="flex items-center justify-between">
+                    <h1 class="text-3xl font-semibold">Upcoming Events</h1>
+
+
+                    <a href="#" class="font-medium text-blue-700 transition-colors inline-flex items-center gap-2">
+                        <span>See all</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </a>
+
+
+                </div>
+
+                <div class="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+                    <div class="space-y-4">
+                        <img src="./assets/images/events/sample_event.jpg" alt="" class="w-full h-60 rounded-2xl object-cover">
+
+                        <p class=" text-gray-600 text-sm">Department · <span>12 minutes ago</span></p>
+
+                        <h1 class="text-xl font-semibold">School Celebrates Annual Cultural Festival 🎉</h1>
+
+                        <p class="text-gray-600 line-clamp-3">
+                            Lorem ipsum odor amet, consectetuer adipiscing elit. Curae cursus pretium nec, eleifend platea venenatis id potenti. Eros ut dignissim pellentesque nec platea. Vehicula natoque ante adipiscing, purus quam laoreet venenatis morbi ad. Condimentum curabitur ad iaculis; in malesuada ultricies curabitur varius. Morbi senectus nibh amet vulputate lacus elit parturient. Sociosqu feugiat varius nibh hendrerit natoque ut. Commodo placerat accumsan adipiscing; eros eleifend rutrum cubilia in.
+                        </p>
+
+                        <p class="text-blue-600 font-medium">News · <span class="text-gray-600 font-normal">4 min read</span></p>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+
+            </div>
+
+        </div>
+
+
+
+    </section>
+
+    <?php include 'footer.php' ?>
+
+
+
+</body>
+
+</html>
