717872cb9993b8a5bd9572b1f3e3e479aede491e
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2025-02-25 07:50:06 +0800

    Refactor sidebar for consistency and clarity.

--- dist/admin/components/sidebar.php
+++ dist/admin/components/sidebar.php
@@ -69,7 +69,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 
                 <li>
-                    <a class="p-2 <?php echo ($current_page == 'campusActivities.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="campusActivities.php">
+                    <a class="p-2 <?php echo ($current_page == 'campusActivities.php') ? 'bg-[#eef0f2]' : ''; ?>" href="campusActivities.php">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                         </svg>
@@ -88,7 +88,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 
                 <li>
-                    <a class="p-2 <?php echo ($current_page == 'analyticsPage.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="analyticsPage.php">
+                    <a class="p-2 <?php echo ($current_page == 'analyticsPage.php') ? 'bg-[#eef0f2]' : ''; ?>" href="analyticsPage.php">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
                         </svg>
@@ -107,8 +107,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
                             School Management
                         </summary>
                         <ul>
-                            <li><a class="<?php echo ($current_page == 'admin_admission_approval.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="admin_admission_approval.php">Admissions</a></li>
-                            <li><a class="<?php echo ($current_page == 'admin_enrollment_approval.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="admin_enrollment_approval.php">Enrollment</a></li>
+                            <li><a class="<?php echo ($current_page == 'admin_admission_approval.php') ? 'bg-[#eef0f2]' : ''; ?>" href="admin_admission_approval.php">Admissions</a></li>
+                            <li><a class="<?php echo ($current_page == 'admin_enrollment_approval.php') ? 'bg-[#eef0f2]' : ''; ?>" href="admin_enrollment_approval.php">Enrollment</a></li>
                             <!-- <li><a>Student List</a></li>
                             <li><a>Teacher List</a></li>
                             <li><a>Fees</a></li> -->
@@ -127,9 +127,9 @@ $current_page = basename($_SERVER['PHP_SELF']);
                             Class Management
                         </summary>
                         <ul>
-                            <li><a class="<?php echo ($current_page == 'class_term.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="class_term.php">Term</a></li>
-                            <li><a class="<?php echo ($current_page == 'class_section.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="class_section.php">Section</a></li>
-                            <li><a class="<?php echo ($current_page == 'class_subject.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="class_subject.php">Subject</a></li>
+                            <li><a class="<?php echo ($current_page == 'class_term.php') ? 'bg-[#eef0f2]' : ''; ?>" href="class_term.php">Term</a></li>
+                            <li><a class="<?php echo ($current_page == 'class_section.php') ? 'bg-[#eef0f2]' : ''; ?>" href="class_section.php">Section</a></li>
+                            <li><a class="<?php echo ($current_page == 'class_subject.php') ? 'bg-[#eef0f2]' : ''; ?>" href="class_subject.php">Subject</a></li>
 
 
                         </ul>
@@ -149,7 +149,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 
                 <li>
-                    <a class="p-2 <?php echo ($current_page == 'theme_customization.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="theme_customization.php">
+                    <a class="p-2 <?php echo ($current_page == 'theme_customization.php') ? 'bg-[#eef0f2]' : ''; ?>" href="theme_customization.php">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z" />
                         </svg>
@@ -159,7 +159,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                 </li>
 
                 <li>
-                    <a class="p-2 <?php echo ($current_page == 'maintenance.php') ? 'bg-[#eef0f2]' : ''; ?>"  href="maintenance.php">
+                    <a class="p-2 <?php echo ($current_page == 'maintenance.php') ? 'bg-[#eef0f2]' : ''; ?>" href="maintenance.php">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
                         </svg>
