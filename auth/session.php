c9491d543a5f594ebb262cf550b49feca80c2250
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2025-03-04 08:44:12 +0800

    OTP failed sent fix, SMTP port and address fix

--- auth/session.php
+++ auth/session.php
@@ -2,7 +2,7 @@
 
 
 if (isset($_SESSION['last_Activity'])) {
-    if (time() - $_SESSION['last_Activity'] > 20) { // 30 minutes
+    if (time() - $_SESSION['last_Activity'] > 1800) { // 30 minutes
         session_unset();
         session_destroy();
         echo "<script>
